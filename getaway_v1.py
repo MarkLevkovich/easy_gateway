74d5c4d892004ca665af7a414499e8218ad660d5
MarkLevkovich <levmarkpost@gmail.com>
2025-12-22 20:20:13 +0300

    Created simple proxy

--- getaway_v1.py
+++ getaway_v1.py
@@ -1,16 +1,31 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response
+from httpx import AsyncClient
 
 
 app = FastAPI(title="Easy Getaway")
 
+TEST_TARGET = "https://httpbin.org/"
 
 @app.api_route("/{catch_path:path}", methods=["GET", "POST"])
 async def cath_all(request: Request, catch_path: str):
-    # tests
-    print(f"Метод: {request.method}")
-    print(f"URL: {request.url}")
-    print(f"Заголовки: {request.headers}")
+    url = f"{TEST_TARGET}/{catch_path}"
     body = await request.body()
-    print(f"Тело: {body}")
+    r_headers =dict(request.headers)
+    
+    async with AsyncClient() as client:
+        response = await client.request(
+            method=request.method,
+            url=url,
+            headers=r_headers,
+            content=body
+        )
+        
+    return Response(
+        content=response.content,
+        status_code=response.status_code,
+        headers=dict(response.headers)
+    )
+    
+    
 
     
